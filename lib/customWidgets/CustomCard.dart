1dd713bc8c1d2cbbc31065cdbf7764626de71b28
Mary Plana <MaryCharmaineLoiPlana@my.unt.edu>
2021-02-27 01:50:00 -0600

    Added dummy data for automotive category. Added tipcard and listoftipcard

--- lib/customWidgets/CustomCard.dart
+++ lib/customWidgets/CustomCard.dart
@@ -30,10 +30,10 @@ class CustomCard extends StatelessWidget {
             decoration: BoxDecoration(
               color: kCardColor,
               borderRadius: BorderRadius.circular(15.0),
-              // border: Border.all(
-              //   width: 1.5,
-              //   color: Colors.white.withOpacity(0.2),
-              // ),
+              border: Border.all(
+                width: 1,
+                color: Colors.white.withOpacity(0.2),
+              ),
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
--- lib/customWidgets/DailyTipCard.dart
+++ lib/customWidgets/DailyTipCard.dart
@@ -23,10 +23,10 @@ class _DailyTipCardState extends State<DailyTipCard> {
           decoration: BoxDecoration(
             color: kCardColor,
             borderRadius: BorderRadius.circular(15.0),
-            // border: Border.all(
-            //   width: 1.5,
-            //   color: Colors.white.withOpacity(0.2),
-            // ),
+            border: Border.all(
+              width: 1,
+              color: Colors.white.withOpacity(0.2),
+            ),
           ),
           child: Padding(
             padding: EdgeInsets.symmetric(
