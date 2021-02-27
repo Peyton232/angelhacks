20e012502c48552d3f3c3d08f045fd886754f26f
Mary Plana <MaryCharmaineLoiPlana@my.unt.edu>
2021-02-26 21:40:11 -0600

    Added blue house and cards

--- lib/pages/WelcomePage.dart
+++ lib/pages/WelcomePage.dart
@@ -6,101 +6,88 @@ import '../pages/homePages/homeHomePage.dart';
 import '../pages/otherPages/otherHomePage.dart';
 import '../pages/techPages/techHomePage.dart';
 
+const kCardColor = Color(0xff9e9e9e);
+const kCardDimensions = 150.0;
+const kAppBarTextStyle = TextStyle(fontSize: 25.0);
+const kHomeBackgroundDimensions = 450.0;
+
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new GridView.count(
-          crossAxisCount: 2,
-          children: <Widget>[
-            FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              padding: EdgeInsets.all(10),
-              child: Text('Car'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CarHomePage(),
-                  ),
-                );
-              },
-            ),
-            FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              padding: EdgeInsets.all(10),
-              child: Text('House'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeHomePage(),
-                  ),
-                );
-              },
-            ),
-            FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              padding: EdgeInsets.all(10),
-              child: Text('Food'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FoodHomePage(),
-                  ),
-                );
-              },
-            ),
-            FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              padding: EdgeInsets.all(10),
-              child: Text('Finance'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FinanceHomePage(),
-                  ),
-                );
-              },
-            ),
-            FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              padding: EdgeInsets.all(10),
-              child: Text('Tech'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TechHomePage(),
-                  ),
-                );
-              },
-            ),
-            FlatButton(
-              textColor: Colors.white,
-              color: Colors.red,
-              padding: EdgeInsets.all(10),
-              child: Text('Other'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => OtherHomePage(),
-                  ),
-                );
-              },
-            ),
-          ],
+      appBar: AppBar(
+        leading: Icon(
+          //TODO: Make into a button
+          Icons.dehaze,
+          color: Colors.white,
         ),
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Adulting",
+          style: kAppBarTextStyle,
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: -100,
+            bottom: 0.0,
+            height: kHomeBackgroundDimensions,
+            width: kHomeBackgroundDimensions,
+            child: Image.asset("images/homeIcon.png"),
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomCard(
+                    cardName: "Home",
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  CustomCard(
+                    cardName: "Finance",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final String cardName;
+  final IconData cardIcon;
+
+  CustomCard({
+    this.cardIcon,
+    @required this.cardName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: kCardDimensions,
+      width: kCardDimensions,
+      decoration: BoxDecoration(
+        color: kCardColor,
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.home,
+            size: 80.0,
+          ),
+          Text(cardName),
+        ],
       ),
     );
   }
