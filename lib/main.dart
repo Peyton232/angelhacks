9331975abf4aee3b4dd54622b737ce1b453fffa2
peyton <peyton.t131@gmail.com>
2021-02-26 19:40:20 -0600

    added button grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'pages/carPages/carHomePage.dart';
+import 'pages/financePages/financeHomePage.dart';
+import 'pages/foodPages/foodHomePage.dart';
+import 'pages/homePages/homeHomePage.dart';
+import 'pages/otherPages/otherHomePage.dart';
+import 'pages/techPages/techHomePage.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -16,16 +22,75 @@ class _State extends State<MyApp> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Name here'),
+        title: new Text('HomePage'),
       ),
       body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Column(
-          children: <Widget>[
-            new Text('Add Widgets Here')
-          ],
-        ),
-      ),
+          padding: new EdgeInsets.all(32.0),
+          child: new GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('Car'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CarHomePage()));
+                },
+              ),
+              FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('House'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeHomePage()));
+                },
+              ),
+              FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('Food'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => FoodHomePage()));
+                },
+              ),
+              FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('Finance'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => FinanceHomePage()));
+                },
+              ),
+              FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('Tech'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TechHomePage()));
+                },
+              ),
+              FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: EdgeInsets.all(10),
+                child: Text('Other'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => OtherHomePage()));
+                },
+              ),
+            ],
+          )),
     );
   }
 }
