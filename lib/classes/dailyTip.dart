e0672ad6fc1d39a1295e12c2c5aaf114e0832a37
davidkt99 <49345822+davidkt99@users.noreply.github.com>
2021-02-28 13:25:23 -0600

    Added tips to Dailytips

--- lib/classes/dailyTip.dart
+++ lib/classes/dailyTip.dart
@@ -4,7 +4,21 @@ import 'dart:math';
 
 class DailyTip {
   var rng = new Random();
-  var tips = ["Water boils at 1000K", "Cleaning a toilet is boring", "If its more than 3 steps, call you parents.", " yoyoyoyo","HAHAHAHA","poopoop","Peepee",];
+  var tips = [
+    "Leaving dryer lint in the lint trap can be a fire hazard.",
+    "replacing your air filter can seriously help those in the house with allergies",
+    "If its more than 3 steps, call you parents.",
+    "Make sure to pay off credit card debt",
+    "Going through and cutting out small expenses can be a great non-intrusive way to lower your spending",
+    "Investing in funds can be a fairly save way to grow your money over time without knowing much about investing",
+    "Update your will, to help protect your loved ones.",
+    "Keep good financal records can make taxes much easier and help increase your tax deductions",
+    "Back up your data",
+    "You can use your smartphone to scan documents",
+    "CTRL + ALT + Delete is a quick way to get to task manager",
+    "CTRL + rolling the scroll wheel on your mouse will change the size of words and pictures on your desktop",
+    "If your computer is not working try unplugging it and plugging it back in",
+  ];
   var curtime;
   var curtip = "If its more than 3 steps, call you parents.";
   var pastDate = new DateTime.now();
